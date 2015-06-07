95e203f571aaf5b7d653d42b5d562ae3cf412ac4
Josep Romero <josep.romero@atinet.es>
2015-06-07 18:45:16 +0200

    Versión 3
    Corrección errores de código que impedían compilar

--- random.js
+++ random.js
@@ -3,8 +3,8 @@ var numero = Math.random();
 
 var str = ' MAYOR que 0,5';
 
-if (numero <= 0-5= {
+if (numero <= 0.5) {
 	str = ' MENOR que 0,5';
 }
 
-console.log('\n' + numero + str '\n');
+console.log('\n' + numero + str + '\n');
